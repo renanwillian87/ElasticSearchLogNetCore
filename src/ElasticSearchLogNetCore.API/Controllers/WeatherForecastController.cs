097968dad1825b6cd572a8c782066833099fb009
renan santos <renanwillian87@gmail.com>
2021-02-11 16:27:00 -0300

    add project

---
+++ src/ElasticSearchLogNetCore.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ElasticSearchLogNetCore.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                var rng = new Random();
+
+                if(rng.Next(0, 5) < 2)
+                {
+                    throw new Exception("Oops what happened?");
+                }
+
+                return Ok(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                })
+                .ToArray());
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, message: "Hey this is a request!");
+                return new StatusCodeResult(500);
+            }
+        }
+    }
+}
---
+++ src/ElasticSearchLogNetCore.API/Program.cs
@@ -0,0 +1,41 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+
+namespace ElasticSearchLogNetCore.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseSerilog(configureLogger: (hostContext, loggerConfiguration) => {
+                    loggerConfiguration
+                        .Enrich.FromLogContext()
+                        //.Enrich.WithMachineName()
+                        .WriteTo.Console()
+                        .WriteTo.Elasticsearch(
+                            new ElasticsearchSinkOptions(new Uri(hostContext.Configuration["ElasticConfiguration:Uri"]))
+                            {
+
+                                IndexFormat = $"{hostContext.Configuration["ApplicationName"]}-logs-{hostContext.HostingEnvironment.EnvironmentName?.ToLower().Replace(".","-")}-{DateTime.UtcNow:yyy-mm}",
+                                AutoRegisterTemplate = true,
+                                NumberOfShards = 2,
+                                NumberOfReplicas = 1
+                            }
+                        )
+                        .Enrich.WithProperty("Environmen", hostContext.HostingEnvironment.EnvironmentName)
+                        .ReadFrom.Configuration(hostContext.Configuration);
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
